eeb1f7822a4cea006fec61941dbdbf27690b0978
catEricksson <arrowess@gmail.com>
2023-10-30 16:13:31 -0700

    Added Users table migration and basic seeds.

---
+++ backend/db/seeders/20231030230346-demo-user.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const { User } = require('../models');
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await User.bulkCreate([
+      {
+        email: 'demo1@user.io',
+        username: 'DemoUser1',
+        hashedPassword: bcrypt.hashSync('password1')
+      },
+      {
+        email: 'demo2@user.io',
+        username: 'DemoUser2',
+        hashedPassword: bcrypt.hashSync('password2')
+      },
+      {
+        email: 'demo3@user.io',
+        username: 'DemoUser3',
+        hashedPassword: bcrypt.hashSync('password3')
+      }
+    ], { validate: true });
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'Users';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      username: { [Op.in]: ['DemoUser1', 'DemoUser2', 'DemoUser3'] }
+    }, {});
+  }
+};
