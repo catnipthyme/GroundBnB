7984dbbfa0e24cd1c8f349a351c1e4db37d156d6
catEricksson <arrowess@gmail.com>
2023-11-02 13:04:57 -0700

    Finished pre-testing Get details of a spot by id

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -8,8 +8,8 @@ const { handleValidationErrors } = require("../../utils/validation");
 const review = require("../../db/models/review");
 const router = express.Router();
 
-
-router.get("/", async(req, res) => {
+// Get all Spots
+router.get("/", async (req, res) => {
   const spots = await Spot.findAll({
     include: [
       {
@@ -53,28 +53,29 @@ router.get("/", async(req, res) => {
   res.json({ spotList });
 });
 
-router.get("/current", requireAuth, async(req, res) => {
-  const {user} = req
+// Get all Spots owned by the Current User
+router.get("/current", requireAuth, async (req, res) => {
+  const { user } = req;
 
   if (!user) {
-    const err = new Error('Authentication required');
-    err.title = 'Authentication required';
-    err.errors = {message: 'Authentication required'};
+    const err = new Error("Authentication required");
+    err.title = "Authentication required";
+    err.errors = { message: "Authentication required" };
     err.status = 401;
-    res.status(401).json(err)
+    res.status(401).json(err);
   }
 
   const userSpots = await Spot.findAll({
-    where: {ownerId: user.id},
+    where: { ownerId: user.id },
     include: [
       {
         model: SpotImage,
       },
       {
         model: Review,
-      }
-    ]
-  })
+      },
+    ],
+  });
 
   let spotList = [];
   userSpots.forEach((spot) => {
@@ -106,11 +107,55 @@ router.get("/current", requireAuth, async(req, res) => {
   });
 
   if (spotList.length === 0) {
-    res.json("Current user has no spots.")
+    res.json("Current user has no spots.");
   }
 
-  res.json(spotList)
+  res.json(spotList);
+});
 
-})
+// Get details of a Spot from an id
+router.get("/:spotId", async (req, res) => {
+  const spot = await Spot.findByPk(req.params.spotId, {
+    include: [
+      { model: SpotImage, attributes: ["id", "url", "preview"] },
+      { model: User, attributes: ["id", "firstName", "lastName"] },
+      { model: Review },
+    ],
+  });
+
+  if (!spot) {
+    const err = new Error("Spot couldn't be found.");
+    res.status(404).json({
+      message: err.message,
+    });
+  }
+
+  selectedSpot = spot.toJSON();
+  const reviews = selectedSpot.Reviews;
+  const numReviews = reviews.length;
+  let totalStars = 0;
+  reviews.forEach((review) => {
+    totalStars += review.stars;
+  });
+  if (!review.length) {
+    (selectedSpot.avgRating = "No reviews available"),
+      (selectedSpot.numReviews = "No reviews available");
+    delete selectedSpot.Reviews;
+  } else {
+    selectedSpot.avgRating = totalStars / numReviews;
+    selectedSpot.numReviews = numReviews;
+    delete selectedSpot.Reviews;
+  }
+
+  selectedSpot.Owner = {
+    id: 2,
+    firstName: "Demoni",
+    lastName: "Userni",
+  };
+
+  delete selectedSpot.User;
+
+  res.json(selectedSpot);
+});
 
 module.exports = router;
